baee6284b079d7ac040a8c4a8d440fb6184eeed7
evan.lightcap <evan.lightcap@cloudkitchens.com>
2023-03-29 11:07:53 -0400

    fix json format

--- src/bpm.py
+++ src/bpm.py
@@ -1,18 +1,18 @@
 import requests
-import json
 import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-URI = os.getenv(HASS_URI)
-TOKEN = os.getenv(HASS_TOKEN)
-OUTFILE = os.getenv('outfile')
+URI = "https://arepo.castleanorak.co:8123/api/states/sensor.owlet_heart_rate"
+TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0YzRlZDlhNDUyODA0ZmM0OTYwYzNiMTBhNWUwZDFkMSIsImlhdCI6MTY4MDA2MzEwOSwiZXhwIjoxOTk1NDIzMTA5fQ.q-u-41l_sN0MInZPWmPTP-ef8cg6DskTavog3RN3m9U"
+OUTFILE = "tester.txt"
 def getBpm():
     hassheaders = {'Authorization': 'BEARER {}'.format(TOKEN)}
     request = requests.get(url=URI, headers=hassheaders)
-    mJson = request.json
+    mJson = request.json()
+    print(mJson)
     bpm = mJson["state"]
     try:
         os.remove(OUTFILE)
