9fd482d5e53a9428ec7cd20950fc7b4c4113bb4a
evan.lightcap <evan.lightcap@cloudkitchens.com>
2023-03-29 00:57:55 -0400

    fix headers

--- src/bpm.py
+++ src/bpm.py
@@ -11,7 +11,7 @@ TOKEN = os.getenv(HASS_TOKEN)
 OUTFILE = os.getenv('outfile')
 def getBpm():
     hassheaders = {'Authorization': 'BEARER {}'.format(TOKEN)}
-    request = requests.get(url=URI, HEADERS=hassheaders)
+    request = requests.get(url=URI, headers=hassheaders)
     mJson = request.json
     bpm = mJson["state"]
     try:
