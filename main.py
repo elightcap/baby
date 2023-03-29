1321daf2961f9f328a69ff3fde0c37a3f3c81641
evan.lightcap <evan.lightcap@cloudkitchens.com>
2023-03-28 23:52:18 -0400

    oops

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.bpm import getBpm
 s = sched.scheduler(time.time, time.sleep)
 def main(sc): 
     getBpm()
-    sc.enter(10, 1, getBpm(), (sc,))
+    sc.enter(10, 1, main, (sc,))
 
 s.enter(0, 1, main, (s,))
 
