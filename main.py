1d150f5fcef4745546f59f9610dd16e6a8d9bde2
evan.lightcap <evan.lightcap@cloudkitchens.com>
2023-03-28 23:51:34 -0400

    fix sched

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.bpm import getBpm
 s = sched.scheduler(time.time, time.sleep)
 def main(sc): 
     getBpm()
-    sc.enter(10, 1, run_five_minutes, (sc,))
+    sc.enter(10, 1, getBpm(), (sc,))
 
 s.enter(0, 1, main, (s,))
 
