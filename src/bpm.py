ecf7ac20074a2a1fdead9e043330525098d8c3cd
evan.lightcap <evan.lightcap@cloudkitchens.com>
2023-03-29 13:59:10 -0400

    fix pathing, volumes

--- src/bpm.py
+++ src/bpm.py
@@ -8,22 +8,24 @@ load_dotenv()
 HEARTURI = ""
 O2URI=""
 TOKEN = ""
-HEARTOUTFILE = ""
-O2OUTFILE=""
+HEARTOUTFILE = "heart.txt"
+O2OUTFILE="o2.txt"
+REL_HEART_PATH = "/usr/src/app/src/files/{}".format(HEARTOUTFILE)
+REL_O2_PATH = "/usr/src/app/src/files/{}".format(O2OUTFILE)
 def getBpm():
     hassheaders = {'Authorization': 'Bearer {}'.format(TOKEN)}
     heartRequest = requests.get(url=HEARTURI, headers=hassheaders)
-    heartJson = heartrequest.json()
+    heartJson = heartRequest.json()
     o2Request = requests.get(url=O2URI, headers=hassheaders)
     o2Json = o2Request.json()
     bpm = heartJson["state"]
     o2 = o2Json["state"]
     try:
-        os.remove(HEARTOUTFILE)
-        os.remove(O2OUTFILE)
+        os.remove(REL_HEART_PATH)
+        os.remove(REL_O2_PATH)
     except OSError:
         pass
-    with open(HEARTOUTFILE, 'w') as f:
+    with open(REL_HEART_PATH, 'w') as f:
         f.write(bpm)
-    with open(O2OUTFILE, "w") as f:
+    with open(REL_O2_PATH, "w") as f:
         f.write(o2)
